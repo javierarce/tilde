24487c7a3f0aa72b3735e9ee93ee396b9eeeec70
Javier Arce <javierarce@gmail.com>
2014-10-07 22:32:43 +0200

    test

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
     username: "javierarce",
     api_key: "52baf5483029010e0e7ece53ac76449e" 
   }).on("listening", function(e, track) {
-    $("listening-js").removeClass("hidden");
+    $(".listening-js").removeClass("hidden");
   });
 
   var myUsername = "javier";
