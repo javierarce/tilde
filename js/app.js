1d7bf32eb9e7c2f9f092cc22185ce9426f2535b1
Javier Arce <javierarce@gmail.com>
2014-11-14 12:27:30 +0100

    adds random string

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@ function numberWithCommas(x) {
 }
 
 function setTildeFavicon() {
+  var randomString = "?" + Math.round(Math.random(10000));
+
   var link  = document.createElement('link');
   link.type = 'image/x-icon';
   link.rel  = 'shortcut icon';
-  link.href = 'img/favicons/tilde.png';
+  link.href = 'img/favicons/tilde.png' + randomString;
 
   document.getElementsByTagName('head')[0].appendChild(link);
   $(document).prop('title', 'Is the ~ Who Is Tall Happy?');
@@ -19,19 +21,20 @@ function setGmailFavicon() {
       var c     = Math.round(Math.random()*13);
       var cT    = c;
       var link  = document.createElement('link');
+      var randomString = "?" + Math.round(Math.random(10000));
       link.type = 'image/x-icon';
       link.rel  = 'shortcut icon';
 
       if (c === 11) {
         c = 50;
         cT = "50+";
-        link.href = 'img/favicons/gmail_' + c + '.png';
+        link.href = 'img/favicons/gmail_' + c + '.png' + randomString;
       } else if (c === 12) {
         c = 100;
         cT = "100+";
-        link.href = 'img/favicons/gmail_' + c + '.png';
+        link.href = 'img/favicons/gmail_' + c + '.png' + randomString;
       } else {
-        link.href = 'img/favicons/gmail_' + c + '.png';
+        link.href = 'img/favicons/gmail_' + c + '.png' + randomString;
       }
 
       document.getElementsByTagName('head')[0].appendChild(link);
