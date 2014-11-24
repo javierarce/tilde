ff47fe685bf1f21b7d3253ce8958aa9d27455512
Javier Arce <javierarce@gmail.com>
2014-11-24 14:07:18 +0100

    change more

--- js/app.js
+++ js/app.js
@@ -345,6 +345,8 @@ function initNLP() {
     var percentageOfChanges = .7;
     var numChanges          = Math.round(textAdjectives.length * percentageOfChanges);
 
+    if (numChanges < 5) numChanges = textAdjectives.length;
+
     for (i = 0; i <= numChanges; i++) {
 
       var a = textAdjectives[Math.round(Math.random() * (textAdjectives.length - 1))];
